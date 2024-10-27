94b1249b5788964c54f1e2a77bfd7a3ed5585d18
Jakub Muzyczuk <jtmuzyczuk@hotmail.com>
2024-10-27 11:57:15 +0100

    Finished homwork from Day 5.

---
+++ Day5/Homework.cs
@@ -0,0 +1,16 @@
+﻿int number = 4566;
+string numberInString = number.ToString();
+char[] letters = numberInString.ToArray();
+
+int[] digitsToCount = new int[10];
+foreach (char c in letters)
+{
+    int digit = c - '0';
+    digitsToCount[digit]++;
+    Console.WriteLine(digitsToCount[digit]);
+}
+
+for (int i = 0; i < digitsToCount.Length; i++)
+{
+    Console.WriteLine($"{i} => {digitsToCount[i]}");
+}
