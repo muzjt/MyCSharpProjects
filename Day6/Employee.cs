21f902653a22644075ab73c5b17664fdb6873e21
Jakub Muzyczuk <jtmuzyczuk@hotmail.com>
2024-10-29 19:29:28 +0100

    Finished homework from day 6.

---
+++ Day6/Employee.cs
@@ -0,0 +1,28 @@
+﻿namespace Day6
+{
+    public class Employee
+    {
+        private List<int> points = new List<int>();
+        public Employee(string name, string surname, string age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public string Age { get; private set; }
+        
+        public int PointsSummary
+        { 
+            get
+            {
+                return this.points.Sum();
+            }
+        }
+        public void AddPoints(int number) 
+        {
+            this.points.Add(number);
+        }
+    }
+}
