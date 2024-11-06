adf92a597db4894cd2786a72d371901274a0bc43
Jakub Muzyczuk <jtmuzyczuk@hotmail.com>
2024-11-06 14:25:40 +0100

    Struktura projektu została przebudowana.

---
+++ ChallengeApp.Tests/EmployeesTests.cs
@@ -0,0 +1,52 @@
+namespace ChallengeApp.Tests
+{
+    public class EmployeesTests
+    {
+        [Test]
+        public void CheckIfPointsAddCorrectly()
+        {
+            // arrange
+            var employee = new Employee("Marcin", "Kowalski", "32");
+            employee.AddPoints(1);
+            employee.AddPoints(2);
+
+            // act
+            var score = employee.PointsSummary;
+
+            // assert
+            Assert.AreEqual(3, score);
+        }
+
+        [Test]
+        public void CheckIfPointsAddAndRemoveCorrectly()
+        {
+            // arrange
+            var employee = new Employee("Marcin", "Kowalski", "32");
+            employee.AddPoints(1);
+            employee.AddPoints(2);
+            employee.AddPoints(-1);
+
+            // act
+            var score = employee.PointsSummary;
+
+            // assert
+            Assert.AreEqual(2, score);
+        }
+
+        [Test]
+        public void CheckIfScoreEqualsZero()
+        {
+            // arrange
+            var employee = new Employee("Marcin", "Kowalski", "32");
+            employee.AddPoints(1);
+            employee.AddPoints(2);
+            employee.AddPoints(-3);
+
+            // act
+            var score = employee.PointsSummary;
+
+            // assert
+            Assert.AreEqual(0, score);
+        }
+    }
+}
