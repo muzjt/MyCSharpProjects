23a838c2d6c61792218c62504494d6a846b76a88
Jakub Muzyczuk <jtmuzyczuk@hotmail.com>
2024-11-06 14:25:40 +0100

    Project structure has been rebuilt.

---
+++ ChallengeApp/Employee.cs
@@ -0,0 +1,35 @@
+﻿namespace ChallengeApp
+{
+    public class Employee
+    {
+        private List<int> points = new List<int>();
+        public Employee(string name, string surname, string age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+
+        public Employee(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public string Age { get; private set; }
+
+        public int PointsSummary
+        {
+            get
+            {
+                return this.points.Sum();
+            }
+        }
+        public void AddPoints(int number)
+        {
+            this.points.Add(number);
+        }
+    }
+}
+
