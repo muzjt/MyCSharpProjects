701d3566bc3364f48d169f445962bd27b03af4aa
Jakub Muzyczuk <jtmuzyczuk@hotmail.com>
2024-11-09 19:29:57 +0100

    Fixed max value in console output.

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -11,8 +11,8 @@ var statistics1 = employee.GetStatisticsWithForEach();
 var statistics2 = employee.GetStatisticsWithFor();
 var statistics3 = employee.GetStatisticsWithDoWhile();
 var statistics4 = employee.GetStatisticsWithWhile();
-Console.WriteLine($"Using GetStatisticsWithForEach method. Average: {statistics1.Average:N2}, Min: {statistics1.Min}, Max: {statistics1.Min}");
-Console.WriteLine($"Using GetStatisticsWithFor method. Average: {statistics2.Average:N2}, Min: {statistics2.Min}, Max: {statistics2.Min}");
-Console.WriteLine($"Using GetStatisticsWithDoWhile method. Average: {statistics3.Average:N2}, Min: {statistics3.Min}, Max: {statistics3.Min}");
-Console.WriteLine($"Using GetStatisticsWithWhile method. Average: {statistics4.Average:N2}, Min: {statistics4.Min}, Max: {statistics4.Min}");
+Console.WriteLine($"Using GetStatisticsWithForEach method. Average: {statistics1.Average:N2}, Min: {statistics1.Min}, Max: {statistics1.Max}");
+Console.WriteLine($"Using GetStatisticsWithFor method. Average: {statistics2.Average:N2}, Min: {statistics2.Min}, Max: {statistics2.Max}");
+Console.WriteLine($"Using GetStatisticsWithDoWhile method. Average: {statistics3.Average:N2}, Min: {statistics3.Min}, Max: {statistics3.Max}");
+Console.WriteLine($"Using GetStatisticsWithWhile method. Average: {statistics4.Average:N2}, Min: {statistics4.Min}, Max: {statistics4.Max}");
 
